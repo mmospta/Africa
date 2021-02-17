5938637180a2e94842fb2a6f7a8526ee99601b43
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-02-17 13:22:23 +0700

    Initial project africa

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Phonthep Aungkanukulwit on 17/2/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
