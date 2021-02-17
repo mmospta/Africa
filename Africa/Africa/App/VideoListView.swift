5e7374f24fc71f1c7b9d12a0bafb08a00b6ad81c
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-02-17 13:53:14 +0700

    Create TabView

---
+++ Africa/Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Phonthep Aungkanukulwit on 17/2/2564 BE.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+  var body: some View {
+    Text("Videos")
+  }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+  static var previews: some View {
+    VideoListView()
+  }
+}
