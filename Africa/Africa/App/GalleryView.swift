83d509af1896acb36170e2a211f258459b29f6c6
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scb.co.th>
2021-03-01 21:16:19 +0700

    MotionAnimationView

--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct GalleryView: View {
   var body: some View {
-    Text("Gallery")
+    ScrollView(.vertical, showsIndicators: false) {
+      Text("Gallery")
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(MotionAnimationView())
   }
 }
 
 struct GalleryView_Previews: PreviewProvider {
   static var previews: some View {
     GalleryView()
+      .previewDevice("iPhone 11 Pro")
   }
 }
